a3b5987f2ab4ff19df943f5e01f9170784acc713
IbrahimKhanMd <patanibrahimkhan789@gmail.com>
2024-09-06 20:18:51 +0530

    Solved the display problem of years

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { EXPERIENCES } from "../constants";
 import { motion } from "framer-motion";
+
 const Experience = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
@@ -13,14 +14,14 @@ const Experience = () => {
       </motion.h1>
       <div>
         {EXPERIENCES.map((Experience, index) => (
-          <div key={index} className="mb-8 flex flex-wraplg:justify-center">
+          <div key={index} className="mb-8 flex flex-col lg:flex-row lg:justify-center">
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
+              className="w-full lg:w-1/4 mb-2 lg:mb-0"
             >
-              <p className="mb-2 text-sm text-neutral-400">{Experience.year}</p>
+              <p className="text-sm text-neutral-400">{Experience.year}</p>
             </motion.div>
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
@@ -29,20 +30,22 @@ const Experience = () => {
               className="w-full max-w-xl lg:w-3/4"
             >
               <h6 className="mb-2 font-semibold">
-                {Experience.role}-{""}
+                {Experience.role}{" "}
                 <span className="text-sm text-purple-100">
-                  {Experience.company}
+                  - {Experience.company}
                 </span>
               </h6>
               <p className="mb-4 text-neutral-400">{Experience.description}</p>
-              {Experience.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
-                >
-                  {tech}
-                </span>
-              ))}
+              <div className="flex flex-wrap">
+                {Experience.technologies.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
             </motion.div>
           </div>
         ))}
