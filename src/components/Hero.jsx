0f54b2711e155bc6534f748ddb58930376e93311
IbrahimKhanMd <patanibrahimkhan789@gmail.com>
2025-10-06 06:19:51 +0530

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/profile2.jpg";
+import profilePic from "../assets/Profile2.jpg";
 import { delay, motion, transform } from "framer-motion";
 
 const container = (delay) => ({
