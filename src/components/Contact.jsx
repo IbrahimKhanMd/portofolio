82c2d2093e27eadcbfc4ba3dc626a275dfffa649
IbrahimKhanMd <patanibrahimkhan789@gmail.com>
2025-12-07 15:41:09 +0530

    resume contact update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -71,7 +71,7 @@ const Contact = () => {
             </h2>
             <div>
               <a
-                href="https://ibrahimkhan-resume.tiiny.site"
+                href="https://drive.google.com/file/d/1s01MCLdgPlgJSL73KNlHWM43n0rLjvFk/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block bg-neutral-700 hover:bg-neutral-600 text-neutral-200 px-6 py-3 rounded-lg transition-all duration-300 hover:shadow-lg hover:shadow-purple-500/30"
